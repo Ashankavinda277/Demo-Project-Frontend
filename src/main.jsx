ab0ee708260948967c4249da6d320e5844369130
Ashan Kavinda <bandaralamak.23@uom.lk>
2025-10-10 11:50:21 +0530

    navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
 import Navbar from './component/common/Navbar/Navbar.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Navbar />,
+    errorElement: <div>Page Not Found</div>,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Navbar />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
