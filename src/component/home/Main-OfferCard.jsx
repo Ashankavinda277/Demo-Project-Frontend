40a9f2713e8bf2d0f0b4ce774381e0e1cd0adc3f
ThulaniThehara <athukoralaatt.23@uom.lk>
2025-10-11 18:01:29 +0530

    hjg

--- src/component/home/Main-OfferCard.jsx
+++ src/component/home/Main-OfferCard.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import '../../css/home.css/Main-Offersgrid.css'
 
 const MainOfferCard = ({ image, title, discount }) => {
   // background div uses the inline background-image so we can blur it independently
-  const bgStyle = image ? { backgroundImage:`url(${image})` } : { backgroundColor: '#ccc' }
+  const bgStyle = image ? { backgroundImage: `url(${image})` } : { backgroundColor: '#ccc' }
 
   return (
     <div className="offer-card">
@@ -16,9 +16,10 @@ const MainOfferCard = ({ image, title, discount }) => {
       </div>
 
       <div className="offer-badge" aria-hidden>
-        <div className="badge-oval">
-          <span className="badge-text">{discount}</span>
-        </div>
+        <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" role="img" aria-hidden>
+          <polygon points="50,5 61.76,33.82 92.80,36.10 69.02,56.18 76.45,86.41 50,70 23.55,86.41 30.98,56.18 7.20,36.10 38.24,33.82" />
+        </svg>
+        <span className="badge-text">{discount}</span>
       </div>
     </div>
   )
