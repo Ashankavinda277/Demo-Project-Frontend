e19cb80a18af8af5d863ffec4ebdb127d3e11dc1
Vishmi Bulathsinhala <vishmibulathsinhala@gmail.com>
2025-10-12 10:47:12 +0530

    added view product page

--- src/main.jsx
+++ src/main.jsx
@@ -1,43 +1,38 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter,RouterProvider } from 'react-router-dom'
-import HomePage from './pages/customer/HomePage.jsx';
-import ProductsPage from './pages/customer/productPage.jsx';
-
-import AboutUs from './pages/customer/aboutUs.jsx';
-
-
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import HomePage from './pages/customer/HomePage.jsx'
+import ProductsPage from './pages/customer/productPage.jsx'
+import AboutUs from './pages/customer/aboutUs.jsx'
+import ProductDetails from './pages/customer/ProductDetails.jsx'
 
 const router = createBrowserRouter([
   {
-    path: '/Home',
+    path: '/',
     element: <HomePage />,
     errorElement: <div>Page Not Found</div>,
   },
-
+  {
+    path: '/Home',
+    element: <HomePage />,
+  },
   {
     path: '/categories/:type',
     element: <ProductsPage />,
   },
-
-   {
+  {
     path: '/products',
-    element: <ProductsPage />, 
-  }
-
-]);
-
+    element: <ProductsPage />,
+  },
+  {
+    path: '/product/:id',
+    element: <ProductDetails />,
+  },
   {
     path: '/aboutUs',
-    element: <AboutUs/>,
-    errorElement: <div>Page Not Found</div>,
+    element: <AboutUs />,
   },
-  
-
-]);
-
-
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
